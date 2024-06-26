171b5f0bf5444fd57365cef0d6bbdc8d1cbfefd4
student <student@WIN-817G5PLIKEO>
2024-06-26 13:59:46 +0900

    0626 tao 1359

--- src/main/java/com/example/demo/controller/HostAccountController.java
+++ src/main/java/com/example/demo/controller/HostAccountController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Host;
@@ -16,6 +17,7 @@ import com.example.demo.model.HostAccount;
 import com.example.demo.repository.HostRepository;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.Transactional;
 
 @Controller
 @RequestMapping("/host")
@@ -34,8 +36,8 @@ public class HostAccountController {
 	public String index() {
 		//セッション破棄、強制返却処理AOP？
 		session.invalidate();
-		System.out.println("Hid:"+haccount.getId());
-		System.out.println("Hname:"+haccount.getName());
+		System.out.println("Hid:" + haccount.getId());
+		System.out.println("Hname:" + haccount.getName());
 
 		return "hostLogin";
 	}
@@ -62,13 +64,79 @@ public class HostAccountController {
 			m.addAttribute("error", "管理者IDまたはパスワードが一致していません");
 			return "hostLogin";
 		}
-		
+
 		haccount.setName(host.getName());
 		haccount.setId(host.getId());
-		System.out.println("Hid:"+haccount.getId());
-		System.out.println("Hname:"+haccount.getName());
-		
+		System.out.println("Hid:" + haccount.getId());
+		System.out.println("Hname:" + haccount.getName());
+
 		return "select";//G202
 
 	}
+
+	@GetMapping("/add/form")
+	public String addForm() {//完成！！！
+
+		return "addHost";//G111利用者登録画面へ
+	}
+
+	@RequestMapping(value = "/add/check", method = { RequestMethod.GET, RequestMethod.POST })
+	public String addCheck(
+			@RequestParam(name = "name", required = false) String name,
+			@RequestParam(name = "password", required = false) String password,
+			//name = というキーワードが、html上に存在しない場合に「null」
+			Model m
+	//みたいな感じ^^
+	) {
+
+		//エラー処理あり
+		//菊：空欄エラー
+		if ((name == null || name.equals("")) || (password == null || password.equals(""))) {
+			m.addAttribute("error", "全項目を入力してください");
+			return "addHost";
+		}
+
+		m.addAttribute("name", name);
+		m.addAttribute("password", password);
+
+		return "checkAddHost";//G112内容確認画面へ
+	}
+
+	@Transactional
+	@RequestMapping(value = "/add/account", method = { RequestMethod.GET, RequestMethod.POST })
+	public String addHost(
+			@RequestParam(name = "name", required = false) String name,
+			@RequestParam(name = "password", required = false) String password,
+			Model m
+	//みたいな感じ^^
+	) {
+		//エラー処理なし
+
+		//正常時
+		//Userのインスタンス作成(名前、email、パスワード)
+		Host host = new Host(name, password);
+
+		//		System.out.println("id:" + user.getId());
+
+		host.setCreatedBy(1);
+
+		//UserRepository.saveAndFlushで作成したインスタンスを保存
+		hostrepository.saveAndFlush(host);
+
+		//新しく作成したuserのidをゲッターで取得
+		//｛注｝
+		Integer id = host.getId();
+
+		host.setCreatedBy(id);
+		hostrepository.saveAndFlush(host);
+
+		m.addAttribute("name", name);
+		m.addAttribute("password", password);
+		//同様にメアド、idを保持
+
+		m.addAttribute("id", id);
+
+		return "doneAddHost";//登録完了画面へ
+	}
+
 }
